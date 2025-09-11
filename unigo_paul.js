06579904976635245a186914ad9da8b5b13d0042
Paul Unigo <paulunig19@gmail.com>
2025-09-11 09:32:11 +0700

    midterm lab act

---
+++ unigo_paul.js
@@ -0,0 +1,41 @@
+const employee=[
+    {
+        name:"Alice",
+        id: 1,
+        salary: 45000,
+        position:['Developer']
+    },
+     {
+        name:"Bob",
+        id: 2,
+        salary: 60000,
+        position:['Manager']
+
+    },
+    {
+        name:"Charlie",
+        id: 3,
+        salary: 38000,
+        position:['Designer']
+    },
+     {
+        name:"Diana",
+        id: 4,
+        salary: 52000,
+        position:['Team Lead']
+
+    },
+     {
+        name:"Ethan",
+        id: 5,
+        salary: 47000,
+        position:['QA Engineer']
+
+    }
+]
+
+if(salary<50000){
+     console.log('The employee will receive 5000')
+ }else{
+     console.log('the employee will not receive a bonus.')
+ }
